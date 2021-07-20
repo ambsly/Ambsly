6b811a65f35f8f2b9258e1689643fb00300a59fc
Shirley Zhang <shirley.a.zhang@gmail.com>
2021-07-20 16:11:01 -0700

    Adding Shirley's feature

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ShirleyComponent1 from './shirleyComponent1.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,7 +9,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <div> Hello World! </div>
+      <div>
+        <div> Hello World! </div>
+        <ShirleyComponent1 />
+      </div>
     )
   }
 }
