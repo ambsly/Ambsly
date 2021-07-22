c9563b20f549fa3329926d5660f717e25fd02eba
Akhil Patel <akhilsf@gmail.com>
2021-07-22 14:58:48 -0700

    client index set up

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/products')
+      .then((results) => {
+        this.setState({
+          products: results.data,
+        });
+        console.log('Products from componentDidMount: ', this.state.products);
+      })
+      .catch((err) => {
+        console.log('Error retrieving product data: ', err);
+      });
   }
 
   render() {
     return (
-      <div> Hello, World! </div>
+      <div>
+
+      </div>
     );
   }
 }
