c67985427d3582414b16c768f73fc17be110e194
Brian Goodall <brian.k.goodall@gmail.com>
2021-07-26 22:33:27 -0700

    average star rating + recommended % dynamically renders on component

--- client/src/components/Ratings_Reviews/overview/overview.jsx
+++ client/src/components/Ratings_Reviews/overview/overview.jsx
@@ -1,47 +1,37 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import StarsBreakdown from './starsbreakdown.jsx';
 import SizeComfortMeter from './sizecomfortmeter.jsx';
 
-const Overview = ({ product, metaData }) => {
-  const totalRatingsCount = (ratings) => {
-    let total = 0;
-    let n = 1;
-    while (n < 6) {
-      total += ratings;
-      n++;
-    }
-    return total;
-  };
+const Overview = ({ metaData }) => {
+  let totalRatingsCount = 0;
 
-  const averageRating = (ratings) => {
-    // pass in metaData.ratings
-    let totalCount = totalRatingsCount(ratings);
+  const averageRating = (metaDataRatings) => {
     let subtotal = 0;
     let n = 1;
     while (n < 6) {
-      subtotal += n * Number(ratings);
+      totalRatingsCount += Number(metaDataRatings[n]);
+      subtotal += n * Number(metaDataRatings[n]);
       n++;
     }
-    return subtotal / totalCount;
-    // let total = totalRatingsCount()
-    // let subtotal = 0;
-    // let n = 1;
-    // while n < 6
-    // subtotal += n * Number(metaData.ratings['n'])
-    // return subtotal/total;
+    return Math.round(subtotal / totalRatingsCount * 2) / 2;
   };
 
-  const recommendPercentageCalculator = (metaData) => {
-    // const recommendedPercent = metaData.recommended.true / (metaData.recommended.true + .false)
-  };
+  const recommendPercentageCalculator = (data) => (
+    Math.round(Number(data.true) / (Number(data.true) + Number(data.false)) * 100)
+  );
 
-  console.log('metaData prop: ', metaData);
   return (
     <div>
-      <span>4  </span>
+      <span>
+        {averageRating(metaData.ratings)}
+        {' '}
+      </span>
       <span>⭐⭐⭐⭐</span>
-      <div>90% of reviews recommend this product</div>
+      <div>
+        {recommendPercentageCalculator(metaData.recommended)}
+        % of reviews recommend this product
+      </div>
       <StarsBreakdown />
       <SizeComfortMeter />
     </div>
