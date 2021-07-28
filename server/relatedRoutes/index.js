4bc25ec9066e0130902670bc7c425ebef7959b06
Mason Powell <slicace1234@gmail.com>
2021-07-27 17:12:36 -0700

    added own server folder

--- server/relatedRoutes/index.js
+++ server/relatedRoutes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const Calls = require('../api');
+const Calls = require('../../api');
 
 const app = express();
 const port = 3000;
