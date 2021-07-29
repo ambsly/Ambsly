afa3e0eee659047e804e89f72675fb17b9102744
Akhil Patel <akhilsf@gmail.com>
2021-07-29 13:34:02 -0700

    More styling on the main image

--- client/src/components/productDetails/display/productDisplay.jsx
+++ client/src/components/productDetails/display/productDisplay.jsx
@@ -2,45 +2,42 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const ImageContainer = styled.div`
+display: flex;
+flex-basis: 625px;
+height: 500px;
 position: relative;
 overflow: hidden;
 margin: 25px;
-// border: 1px solid black;
+box-shadow: 0 0 3px;
 `;
 
 const MainImage = styled.img`
+position: absolute;
 width: 625px;
 height: 500px;
 object-fit: cover;
 transition: 0.4s linear;
 `;
 
-// const ExpandButton = styled.span`
-// background: rgba(255, 255, 255, 0.50);
-// width: 30px;
-// height: 30px;
-// border: none;
-// border-radius: 50%;
-// color: rgba(50, 50, 50);
-// font-size: 18px;
-// text-align: center;
-// position: absolute;
-// top: 10%;
-// left: 10%;
-// `;
+const ScrollBackground = styled.div`
+position: absolute;
+height: 80px;
+width: 625px;
+top: 420px;
+background-color: black;
+opacity: 0.2;
+`;
 
 const ScrollMenu = styled.div`
-position: absolute;
-top: 440px;
-left: 50px;
+position: relative;
+top: 210px;
+max-width: 525px;
 margin: auto;
-width: 525px;
-overflow-x: scroll;
+overflow-x: auto;
 white-space: nowrap;
-scrollbar-color: #90A4AE;
 
   &::-webkit-scrollbar {
-    width: 1px;
+    width: 10px;
   }
 
   &::-webkit-scrollbar-track {
@@ -78,7 +75,6 @@ const ProductDisplay = ({ currentStyle, mainImageKey, changeImage }) => {
   }
 
   const [expanded, setExpand] = useState([false]);
-  // const [expandIcon, setExpandIcon] = useState('✛');
 
   const handleExpand = (e) => {
     const xPos = (e.clientX - 138);
@@ -89,12 +85,6 @@ const ProductDisplay = ({ currentStyle, mainImageKey, changeImage }) => {
     } else {
       setExpand([false]);
     }
-
-    // if (expandIcon === '✛') {
-    //   setExpandIcon('✕');
-    // } else {
-    //   setExpandIcon('✛');
-    // }
   };
 
   const imageSelector = (e) => {
@@ -104,36 +94,23 @@ const ProductDisplay = ({ currentStyle, mainImageKey, changeImage }) => {
   const currentImage = currentStyle.photos[mainImageKey].url || currentStyle.photos[0].url;
 
   return (
-    <div id="gallery">
-      <ImageContainer>
-        <MainImage
-          src={currentImage}
-          alt=""
-          onClick={handleExpand}
-          onMouseLeave={handleExpand}
-          style={{
-            cursor: expanded[0] ? 'zoom-out' : 'zoom-in',
-            transform: expanded[0] ? 'scale(2.5)' : 'scale(1)',
-            transformOrigin: expanded[0] ? `${expanded[1]}px ${expanded[2]}px` : undefined,
-          }}
-        />
-        <ScrollMenu>
-          {currentStyle.photos.map((image, key) => {
-            if (image.url === currentImage) {
-              return (
-                <ImagePreview
-                  type="image"
-                  src={image.thumbnail_url}
-                  alt=""
-                  id={key}
-                  key={key}
-                  onClick={imageSelector}
-                  style={{
-                    opacity: 0.5,
-                  }}
-                />
-              );
-            }
+    <ImageContainer>
+      <MainImage
+        src={currentImage}
+        alt=""
+        onClick={handleExpand}
+        onMouseLeave={handleExpand}
+        style={{
+          cursor: expanded[0] ? 'zoom-out' : 'zoom-in',
+          transform: expanded[0] ? 'scale(2.5)' : 'scale(1)',
+          transformOrigin: expanded[0] ? `${expanded[1]}px ${expanded[2]}px` : undefined,
+          zIndex: expanded[0] ? '1000' : undefined,
+        }}
+      />
+      <ScrollBackground />
+      <ScrollMenu>
+        {currentStyle.photos.map((image, key) => {
+          if (image.url === currentImage) {
             return (
               <ImagePreview
                 type="image"
@@ -142,12 +119,26 @@ const ProductDisplay = ({ currentStyle, mainImageKey, changeImage }) => {
                 id={key}
                 key={key}
                 onClick={imageSelector}
+                style={{
+                  opacity: 0.5,
+                  boxShadow: '0 0 3px white',
+                }}
               />
             );
-          })}
-        </ScrollMenu>
-      </ImageContainer>
-    </div>
+          }
+          return (
+            <ImagePreview
+              type="image"
+              src={image.thumbnail_url}
+              alt=""
+              id={key}
+              key={key}
+              onClick={imageSelector}
+            />
+          );
+        })}
+      </ScrollMenu>
+    </ImageContainer>
   );
 };
 
