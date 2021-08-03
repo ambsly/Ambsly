aaf00f5945330419953a813a9301ba364b1b478c
Brian Goodall <brian.k.goodall@gmail.com>
2021-08-02 17:07:26 -0700

    made progress on add review modal

--- client/src/components/Ratings_Reviews/reviewList/addReviewModal.jsx
+++ client/src/components/Ratings_Reviews/reviewList/addReviewModal.jsx
@@ -22,26 +22,95 @@ const ModalForm = styled.div`
   left: 50%;
   top: 30%;
   transform: translate(-50%, -30%);
-  overflow: hidden;
-  overflow-x: hidden;
-  overflow-y: hidden;
 
   z-Index: 1000;
   background-color: white;
   border: solid 2px black;
   border-radius: 5px;
-  padding: 10px;
+  padding: 30px;
+  width: 720px;
+  min-width: 30%;
 `;
 
-const Label = styled.label``;
+const Title = styled.div`
+  font-size: 20px;
+  margin-bottom: 5px;
+  /* padding: 10px; */
+`;
 
+const Subtitle = styled.div`
+  display: block;
+  font-size: 14px;
+  margin-bottom: 8px;
+`;
+
+const Input = styled.input`
+  overflow: hidden;
+  text-align: left;
+  padding: 5px 5px 5px 10px;
+  margin: 8px 0 8px 0;
+`;
+
+const RadioInput = styled.input`
+  margin: 8px 30px 0px 6px;
+`;
+
+const ReviewBody = styled.textarea`
+  overflow: auto;
+  padding: 5px 5px 5px 10px;
+  margin: 8px 0 8px 0;
+  height: 160px;
+  width: 660px;
+  min-width: 70%;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-top: 20px;
+`;
+
+// handle submit
+// state will be an obj with username: '', other placeholders
+// then on each field input, state will update on Change to populate empty strings
+// that state obj will be packaged up and sent in an axios.post
+
+// also include x on top right in case they dont' wanna submit review
 const AddReviewModal = ({ open, onClose }) => {
   if (!open) return null;
   return ReactDOM.createPortal(
     <Container>
       <Overlay />
       <ModalForm>
-        <button onClick={onClose} type="submit">Click to close me</button>
+        <Title>Write a Review</Title>
+        <Subtitle>{`productName`}</Subtitle>
+        <form>
+          <Label htmlFor="rating">Rating:</Label>
+          <Input type="number" min="1" max="5" id="rating" />
+
+          <Label htmlFor="title">Title:</Label>
+          <Input type="text" id="title" />
+
+          <Label htmlFor="summary">Review (50 - 1000 characters):</Label>
+          <ReviewBody required minLength="50" maxLength="1000" id="summary" />
+
+          <div>Would you recommend this product?</div>
+          <br />
+          <label htmlFor="yes">Yes</label>
+          <RadioInput type="radio" id="yes" name="recommend" />
+
+          <label htmlFor="no">No</label>
+          <RadioInput type="radio" id="no" name="recommend" />
+
+          <Label htmlFor="username">Your Name:</Label>
+          <Input type="text" id="username" />
+
+          <Label htmlFor="email">Email:</Label>
+          <Input type="email" id="email" />
+
+          <Label className="button" />
+          <button onClick={onClose} type="submit">Post</button>
+
+        </form>
       </ModalForm>
     </Container>,
     document.getElementById('portal'),
